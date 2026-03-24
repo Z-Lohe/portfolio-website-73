9e1cbd2af4657380d5956335e7d87eacb8413bd0
Z-Lohe <loheziza0@gmail.com>
2026-03-25 05:06:04 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.error(err));
 
 // Schema
 const messageSchema = new mongoose.Schema({
